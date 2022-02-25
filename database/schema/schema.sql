5f0907d879df63fec24a1d51405d9940d68e610a
arturlevin <84621395+arturlevin@users.noreply.github.com>
2022-02-25 23:45:02 +0500

    Add files via upload
    Схема данных

--- database/schema/schema.sql
+++ database/schema/schema.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.3
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1:3306
--- Время создания: Окт 27 2021 г., 13:47
--- Версия сервера: 5.6.37
--- Версия PHP: 5.5.38
+-- Время создания: Фев 18 2022 г., 22:22
+-- Версия сервера: 10.5.11-MariaDB
+-- Версия PHP: 8.0.14
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -30,10 +29,9 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `accounts` (
   `id` int(11) NOT NULL,
-  `name` text,
-  `surname` text,
+  `name` text DEFAULT NULL,
   `nickname` varchar(255) DEFAULT NULL,
-  `user_pic` blob,
+  `user_pic` text DEFAULT NULL,
   `user_id` int(11) DEFAULT NULL,
   `role_id` int(11) DEFAULT NULL,
   `created_at` datetime DEFAULT NULL,
@@ -77,6 +75,8 @@ CREATE TABLE `baskets` (
 CREATE TABLE `companies` (
   `id` int(11) NOT NULL,
   `name_company` varchar(255) DEFAULT NULL,
+  `description_company` varchar(255) NOT NULL,
+  `logotype_company` text NOT NULL,
   `created_at` datetime DEFAULT NULL,
   `updated_at` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -121,7 +121,7 @@ CREATE TABLE `games` (
   `name` varchar(255) DEFAULT NULL,
   `price` float DEFAULT NULL,
   `description` varchar(455) DEFAULT NULL,
-  `game_pic` blob,
+  `game_pic` text DEFAULT NULL,
   `archive` tinyint(1) DEFAULT NULL,
   `genre_id` int(11) DEFAULT NULL,
   `company_id` int(11) DEFAULT NULL,
@@ -154,6 +154,20 @@ CREATE TABLE `orders` (
   `account_id` int(11) DEFAULT NULL,
   `created_at` datetime DEFAULT NULL,
   `updated_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `phinxlog`
+--
+
+CREATE TABLE `phinxlog` (
+  `version` bigint(20) NOT NULL,
+  `migration_name` varchar(100) DEFAULT NULL,
+  `start_time` timestamp NULL DEFAULT NULL,
+  `end_time` timestamp NULL DEFAULT NULL,
+  `breakpoint` tinyint(1) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -206,7 +220,10 @@ CREATE TABLE `update_balances` (
 
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
+  `login` varchar(255) NOT NULL,
   `pass` varchar(255) DEFAULT NULL,
+  `name` text NOT NULL,
+  `surname` text NOT NULL,
   `phone` varchar(255) DEFAULT NULL,
   `created_at` datetime DEFAULT NULL,
   `updated_at` datetime DEFAULT NULL
@@ -282,6 +299,12 @@ ALTER TABLE `orders`
   ADD KEY `discount_on_game_id` (`discount_on_game_id`);
 
 --
+-- Индексы таблицы `phinxlog`
+--
+ALTER TABLE `phinxlog`
+  ADD PRIMARY KEY (`version`);
+
+--
 -- Индексы таблицы `return_orders`
 --
 ALTER TABLE `return_orders`
@@ -317,66 +340,79 @@ ALTER TABLE `users`
 --
 ALTER TABLE `accounts`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
 --
 -- AUTO_INCREMENT для таблицы `balances`
 --
 ALTER TABLE `balances`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
 --
 -- AUTO_INCREMENT для таблицы `baskets`
 --
 ALTER TABLE `baskets`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
 --
 -- AUTO_INCREMENT для таблицы `companies`
 --
 ALTER TABLE `companies`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
 --
 -- AUTO_INCREMENT для таблицы `discounts`
 --
 ALTER TABLE `discounts`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
 --
 -- AUTO_INCREMENT для таблицы `discount_on_games`
 --
 ALTER TABLE `discount_on_games`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
 --
 -- AUTO_INCREMENT для таблицы `games`
 --
 ALTER TABLE `games`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
 --
 -- AUTO_INCREMENT для таблицы `genres`
 --
 ALTER TABLE `genres`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
 --
 -- AUTO_INCREMENT для таблицы `orders`
 --
 ALTER TABLE `orders`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
 --
 -- AUTO_INCREMENT для таблицы `return_orders`
 --
 ALTER TABLE `return_orders`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
 --
 -- AUTO_INCREMENT для таблицы `roles`
 --
 ALTER TABLE `roles`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
 --
 -- AUTO_INCREMENT для таблицы `update_balances`
 --
 ALTER TABLE `update_balances`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
 --
 -- AUTO_INCREMENT для таблицы `users`
 --
 ALTER TABLE `users`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
 --
 -- Ограничения внешнего ключа сохраненных таблиц
 --
@@ -393,47 +429,6 @@ ALTER TABLE `accounts`
 --
 ALTER TABLE `balances`
   ADD CONSTRAINT `balances_ibfk_1` FOREIGN KEY (`account_id`) REFERENCES `accounts` (`id`);
-
---
--- Ограничения внешнего ключа таблицы `baskets`
---
-ALTER TABLE `baskets`
-  ADD CONSTRAINT `baskets_ibfk_1` FOREIGN KEY (`order_id`) REFERENCES `orders` (`id`),
-  ADD CONSTRAINT `baskets_ibfk_2` FOREIGN KEY (`account_id`) REFERENCES `accounts` (`id`);
-
---
--- Ограничения внешнего ключа таблицы `discount_on_games`
---
-ALTER TABLE `discount_on_games`
-  ADD CONSTRAINT `discount_on_games_ibfk_1` FOREIGN KEY (`discount_id`) REFERENCES `discounts` (`id`),
-  ADD CONSTRAINT `discount_on_games_ibfk_2` FOREIGN KEY (`game_id`) REFERENCES `games` (`id`);
-
---
--- Ограничения внешнего ключа таблицы `games`
---
-ALTER TABLE `games`
-  ADD CONSTRAINT `games_ibfk_1` FOREIGN KEY (`genre_id`) REFERENCES `genres` (`id`),
-  ADD CONSTRAINT `games_ibfk_2` FOREIGN KEY (`company_id`) REFERENCES `companies` (`id`);
-
---
--- Ограничения внешнего ключа таблицы `orders`
---
-ALTER TABLE `orders`
-  ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`account_id`) REFERENCES `accounts` (`id`),
-  ADD CONSTRAINT `orders_ibfk_2` FOREIGN KEY (`discount_on_game_id`) REFERENCES `discount_on_games` (`id`);
-
---
--- Ограничения внешнего ключа таблицы `return_orders`
---
-ALTER TABLE `return_orders`
-  ADD CONSTRAINT `return_orders_ibfk_1` FOREIGN KEY (`order_id`) REFERENCES `orders` (`id`);
-
---
--- Ограничения внешнего ключа таблицы `update_balances`
---
-ALTER TABLE `update_balances`
-  ADD CONSTRAINT `update_balances_ibfk_1` FOREIGN KEY (`balance_id`) REFERENCES `balances` (`id`),
-  ADD CONSTRAINT `update_balances_ibfk_2` FOREIGN KEY (`account_id`) REFERENCES `accounts` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
