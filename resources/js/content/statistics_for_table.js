2a4cfb1758eb8691e50d1aa053140475f307b789
Sergey <sergeybulankin@gmail.com>
2022-03-02 15:15:24 +0300

    Настроен базовый валидатор для форм

--- resources/js/content/statistics_for_table.js
+++ resources/js/content/statistics_for_table.js
@@ -2,5 +2,5 @@ export function addNewValueToCountContent()
 {
     let count = document.getElementById('count_values').innerText;
 
-    document.getElementById('count_values').innerHTML = parseInt(count) + 1;
+    if (count != null) { document.getElementById('count_values').innerHTML = parseInt(count) + 1};
 }
