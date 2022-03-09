49bf32f86212b6220ba10a1d6c6a0a3c4e3ccd5e
sergeybulankin <sergeybulankin@gmail.com>
2022-03-09 18:20:42 +0500

    Реализована работа с получением данных от сервера для корзины

--- resources/js/db/send.js
+++ resources/js/db/send.js
@@ -1,12 +1,16 @@
-export function sendData(formData, action) {
+export function sendData(formData, action, header = "") {
+    // если желаем получить данные из корзины, то подключаем
+    // заголовок
+    var h = new Headers();
+    if (header) {
+        h.append("Content-Type", header);
+    }
+
     return async (formData) => {
         const fetchResponse = await fetch(action, {
             method: 'POST',
             body: formData,
-            headers: {
-                'X-Requested-With': 'XMLHttpRequest',
-                'content-type': 'application/x-www-form-urlencoded'
-            }
+            headers: h
         });
         if (!fetchResponse.ok) {
             throw new Error(`Ошибка по адресу ${url}, статус ошибки ${fetchResp.status}`);
