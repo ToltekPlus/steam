4fcb49bed5c4dfedd4eef9aceb9490473fedffde
GaynetdinovR <92984940+GaynetdinovR@users.noreply.github.com>
2022-03-14 18:47:40 +0500

    Добавлена форма баланса

---
+++ app/views/administrator/balances/index.php
@@ -0,0 +1,18 @@
+<?php \Core\View::renderHeader()?>
+<section class="columns is-full is-centered is-vertical catalog">
+        <section class="column is-8 filter">
+            <? foreach($balances as $balance):?>
+              <h2>Баланс: <?=$balance->balance?>RUB</h2>
+            <? endforeach;?>
+            <form action="balances/store" method="POST" class="form">
+                <div class="form-control">
+                        <input type="hidden" value="<?=$balance->id?>" name="id">
+                        <input type="text" name="sum" id="sum">
+                    <div class="buttons">
+                        <button class="button is-success is-light" type="submit">Пополнить</button>
+                    </div>
+                </div>
+            </form>
+        </section>
+    </section>
+<?php \Core\View::renderFooter()?>
