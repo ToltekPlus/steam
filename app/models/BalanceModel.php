d34fee0be073717582fc689381fae7a8451c6c30
GaynetdinovR <92984940+GaynetdinovR@users.noreply.github.com>
2022-03-14 18:49:30 +0500

    Добавлена модель баланса
    Методы all, delete, find, store.

---
+++ app/models/BalanceModel.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Model;
+
+use Core\Model;
+use Core\DataBuilder;
+
+class BalanceModel extends Model{
+    
+    protected $table = 'balances';
+
+    /*
+     * Сбор всех данных из таблицы баланса
+     */
+    public function all() : array
+    {
+        return $this->getAll($this->table);
+    }
+
+    /*
+     * Добавление данных в таблицу баланса
+     */
+    public function store($args)
+    {
+	return $this->storeToTable($this->table, $args);
+    }
+	
+    /*
+     * Забирает id
+     */
+    public function find($id)
+    {
+    	$balance = $this->getByIdFromTable($this->table, $id);
+        return array_shift($balance);
+    }
+
+    /*
+     * Удаление данных из таблицы по id
+     */
+    public function delete($id)
+    {
+        $args = ['id' => $id];
+        return $this->deleteFromTable($this->table, $args);
+    }
+
+}
