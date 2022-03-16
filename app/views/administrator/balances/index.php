3f14f125a0bad52a85022213faff57a697d4fc1a
GaynetdinovR <92984940+GaynetdinovR@users.noreply.github.com>
2022-03-16 18:16:14 +0500

    Update index.php

--- app/views/administrator/balances/index.php
+++ app/views/administrator/balances/index.php
@@ -4,11 +4,11 @@
             <? foreach($balances as $balance):?>
               <h2>Баланс: <?=$balance->balance?>RUB</h2>
             <? endforeach;?>
-            <form action="balances/store" method="POST" class="form">
+            <form action="store" method="POST" class="form">
                 <div class="form-control">
                         <input type="hidden" value="<?=$balance->id?>" name="id">
                         <input type="text" name="sum" id="sum">
-                    <div class="buttons">
+                    <div style="margin-left: 43%" class="buttons">
                         <button class="button is-success is-light" type="submit">Пополнить</button>
                     </div>
                 </div>
