c93a56fc85df9b23fa4f02c1ff5aafd94be965f3
GaynetdinovR <92984940+GaynetdinovR@users.noreply.github.com>
2022-03-19 01:01:38 +0500

    Полностью переделана главная страница баланса

--- app/views/administrator/balances/index.php
+++ app/views/administrator/balances/index.php
@@ -2,17 +2,28 @@
 <section class="columns is-full is-centered is-vertical catalog">
         <section class="column is-8 filter">
             <? foreach($balances as $balance):?>
-              <h2>Баланс: <?=$balance->balance?>RUB</h2>
-            <? endforeach;?>
-            <form action="store" method="POST" class="form">
-                <div class="form-control">
-                        <input type="hidden" value="<?=$balance->id?>" name="id">
-                        <input type="text" name="sum" id="sum">
-                    <div style="margin-left: 43%" class="buttons">
-                        <button class="button is-success is-light" type="submit">Пополнить</button>
-                    </div>
+              <div style='display:block; margin: 2% 15% 0% 10%;'>
+                  <h2>Баланс: </h2>
+                  <h1><?=$balance->balance?>RUB</h1>
+              </div>
+              <div style="display:block">
+                    <form action="show" method="POST" style="margin: 5% 0% 10% 0%;">
+                        <div class="form-control">
+                            <div class='buttons'>
+                                <button class="button is-success is-light" type="submit">Пополнить</button>
+                            </div>
+                        </div>
+                    </form>
+                    <form action="history" method="POST">
+                        <div class="form-control">
+                            <div class='buttons'>
+                                <button class="button is-success is-light" type="submit">История пополнений</button>
+                            </div>
+                        </div>
+                    </form>
                 </div>
             </form>
+        <? endforeach;?>
         </section>
     </section>
 <?php \Core\View::renderFooter()?>
