fd7973f831c52585551f7a3dda4797fe42ae61b9
s <84621035+SergeyAksakov@users.noreply.github.com>
2022-05-07 11:49:04 +0500

    Add files via upload

--- app/policies/UserRolePolicyAdmin.php
+++ app/policies/UserRolePolicyAdmin.php
@@ -22,7 +22,7 @@ class UserRolePolicy {
 
         if ((int)$this->role === 3) {
             return true;
-        }else {
+        } else {
             die(View::render('errors/403.php'));
         }
     }
