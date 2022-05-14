2ee10f7c04929fb95a8bf2c0524c91fa58724c0a
Danila321 <danil.hasanov220@gmail.com>
2022-05-14 16:46:16 +0500

    Добавлено окно предупреждения

--- resources/js/content/delete_userpic.js
+++ resources/js/content/delete_userpic.js
@@ -5,27 +5,39 @@ import { redirect } from '../db/redirect';
 const button = document.querySelector('#deleteUserpic');
 if (button) {
   button.addEventListener('click', function () {
-    const header = {
-      header: 'application/x-www-form-urlencoded',
-    };
+    Swal.fire({
+      title: 'Вы хотите удалить фото профиля?',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      cancelButtonText: 'Отмена',
+      confirmButtonText: 'Да',
+    }).then(result => {
+      if (result.isConfirmed) {
+        const header = {
+          header: 'application/x-www-form-urlencoded',
+        };
 
-    const path = 'delete_userpic';
+        const path = 'delete_userpic';
 
-    const data_send = {};
+        const data_send = {};
 
-    const send = sendData(data_send, path, header.header);
-    send(data_send, path, header.header)
-      .then(response => {
-        if (response.trim() == '') {
-          // запускаем всплывающее окно с сообщением, что все ок
-          notification('Данные обновлены');
-        } else {
-          // запускаем всплывающее окно с сообщением, что произошла ошибка
-          notification('Произошла ошибка', 'error');
-        }
-      })
-      .catch(error => console.error(error));
+        const send = sendData(data_send, path, header.header);
+        send(data_send, path, header.header)
+          .then(response => {
+            if (response.trim() == '') {
+              // запускаем всплывающее окно с сообщением, что все ок
+              notification('Данные обновлены');
+            } else {
+              // запускаем всплывающее окно с сообщением, что произошла ошибка
+              notification('Произошла ошибка', 'error');
+            }
+          })
+          .catch(error => console.error(error));
 
-    setTimeout(redirect('/account/edit'), 2000);
+        setTimeout(redirect('/account/edit'), 2000);
+      }
+    });
   });
 }
