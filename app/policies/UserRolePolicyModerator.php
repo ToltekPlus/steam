933af4357137cd0dd6d8c380421f3fcbbc219cf5
s <84621035+SergeyAksakov@users.noreply.github.com>
2022-05-14 10:18:32 +0500

    policy

---
+++ app/policies/UserRolePolicyModerator.php
@@ -0,0 +1,28 @@
+<?php
+namespace App\Policy;
+
+use App\Model\UserRoleModel;
+use Core\View;
+
+class UserRolePolicy {
+    /**
+     * @var array
+     */
+    protected $role;
+
+    /**
+     * UserRolePolicy constructor.
+     * @throws \Exception
+     */
+    public function __construct()
+    {
+        $role = new UserRoleModel();
+        $this->role = $role->getByAuthId();
+
+        if ((int)$this->role === 2) {
+            return true;
+        } else {
+            die(View::render('errors/403.php'));
+        }
+    }
+}
